a549efb9289368f1fedf4bb89cc2207f1838ec86
KurtisMelkisedec <kurtismelkisoft@gmail.com>
2025-12-30 22:40:11 +0000

    first commit

---
+++ src/main/java/com/ms1/ms1userservice/Ms1UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms1.ms1userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms1UserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ms1/ms1userservice/Ms1UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms1.ms1userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
